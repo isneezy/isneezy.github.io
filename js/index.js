05ad1790854c076d1762fa4deeb8b9e3931d0e4b
Ivan <vilanculoivan@technopluz.com>
2015-11-12 11:48:29 +0200

    Added Smoot Scrolling Feature

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 //@prepros-prepend lib/jquery/dist/jquery.min.js
+//@prepros-prepend lib/jquery-mousewheel/jquery.mousewheel.min.js
+//@prepros-prepend lib/simplr-smoothscroll/lib/jquery.simplr.smoothscroll.min.js
 //@prepros-prepend functions.js
 //@prepros-prepend menu.js
+
+
+$(function () {
+  $.srSmoothscroll({
+    // defaults
+    step: 55,
+    speed: 400,
+    ease: 'swing',
+    target: $('body'),
+    container: $(window)
+  })
+})
