5d16364a5e9030428a98af4189fe62892140a2f0
isneezy <vilanculoivan@technopluz.com>
2015-11-12 15:58:06 +0200

    Added validation on contact php and updated profile photo

--- contact.php
+++ contact.php
@@ -4,9 +4,25 @@ if($_POST):
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 
+	$code = 1;
+	$responce_message;
+
+	if(!filter_var($email, FILTER_VALIDATE_EMAIL)):
+		$responce_message =  "Email invalido, por favor digite um email valido e tente novamente";
+	elseif(!$name): $responce_message = "Pror favor preencha o seu nome e tente novamente!";
+	elseif(!$message): $responce_message = "Ups, acho que se esqueceu de deixar a sua mensagem, tente novamente!";
+	else:
+		$responce_message = "Obrigado por me contactar, irei responde-lo dentro em breve!";
+		$code = 0;
+	endif;
+
 	$responce = array(
-		'code'=>1,
-		'message'=>'Obrigado por me contactar, irei responde-lo dentro em breve!'
+		'code'=>$code,
+		'message'=> $responce_message
 	);
 	echo json_encode($responce);
+endif;
+
+if($_GET):
+	echo "Metodo nao suportado!";
 endif;
